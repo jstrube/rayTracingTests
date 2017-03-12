b63a877a4ec298ca811777b319c642d28f9e7976
Jan Strube <jan.strube@gmail.com>
2017-03-11 23:17:31 -0800

    adding loop until readout plane is hit

--- iTOP_toys.jl
+++ iTOP_toys.jl
@@ -15,7 +15,7 @@ front = Plane(Coord(0.0, dY, 0.0),  unitize(Coord(0.0, 1.0, 0.0)),  "reflective"
 back =  Plane(Coord(0.0, -dY, 0.0), unitize(Coord(0.0, -1.0, 0.0)), "reflective")
 right = Plane(Coord(0.0, 0.0, dZ),  unitize(Coord(0.0, 0.0, 1.0)),  "reflective")
 # PMT readout is defined to be on the left side
-left =  Plane(Coord(0.0, 0.0, -dZ), unitize(Coord(0.0, 0.0, -1.0)), "transmissive")
+left =  Plane(Coord(0.0, 0.0, -dZ), unitize(Coord(0.0, 0.0, -1.0)), "transmission")
 
 # define the geometry
 cells = Array{Cell}(0)
@@ -37,10 +37,15 @@ bounding_box = Box(Coord(-dX, -dY, -dZ), Coord(dX, dY, dZ))
 geometry = Geometry(cells, bounding_box)
 #plot_cell_2D(geometry, Box(Coord(-dX, -dY, 0), Coord(dX, dY, 0)), 1000, 2)
 
-ray = generate_random_ray(geometry.bounding_box)
-println(ray)
-# Perform a single step of ray tracing on the geometry
-new_ray, id, boundary_type = find_intersection(ray, geometry)
-# Compute distance travelled by the ray
-distance = magnitude( new_ray.origin - ray.origin )
-println("Ray moved ", distance, " [cm] before hitting a ", boundary_type, " boundary")
+for x in 1:10000
+    ray = generate_random_ray(geometry.bounding_box)
+    # Perform a single step of ray tracing on the geometry
+    new_ray, id, boundary_type = find_intersection(ray, geometry)
+    # Compute distance travelled by the ray
+    distance = magnitude( new_ray.origin - ray.origin )
+    while boundary_type != "transmission"
+        distance += magnitude( new_ray.origin - ray.origin )
+        new_ray, id, boundary_type = find_intersection(new_ray, geometry)
+    end
+    println("Ray moved ", distance, " [cm] before hitting a ", boundary_type, " boundary")
+end
